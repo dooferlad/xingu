e62ecd0d788bec3251a5d4853b50e509e0b12db5
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2021-07-13 17:35:12 +0100

    Stop printing out which config file is in use because it breaks the generation of shell completion code.

--- cmd/root.go
+++ cmd/root.go
@@ -86,8 +86,8 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		os.Exit(1)
 	}
 
 	if awsProfile != "" {
